31090af32184acf4cd976910aa69e4ba741beba0
inakidml <inaki.diaz@ikasle.egibide.org>
2017-03-11 21:16:54 +0100

    Revert "Merged branch master into hilo"
    This reverts commit 2537823d50c228a2c8aa1ad4b0cea5a6b8470c9a.

--- tcpSocket/src/tcpsocket/MainWindow.java
+++ tcpSocket/src/tcpsocket/MainWindow.java
@@ -9,12 +9,12 @@ package tcpsocket;
  *
  * @author inaki
  */
-public class Window extends javax.swing.JFrame {
+public class MainWindow extends javax.swing.JFrame {
 
     /**
      * Creates new form MainWindow
      */
-    public Window() {
+    public MainWindow() {
         initComponents();
     }
 
@@ -30,15 +30,15 @@ public class Window extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
         jLabel2 = new javax.swing.JLabel();
-        jTextFieldIp = new javax.swing.JTextField();
+        jTextField1 = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        jTextFieldPort = new javax.swing.JTextField();
-        jButtonSetIp = new javax.swing.JButton();
+        jTextField2 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextAreaRec = new javax.swing.JTextArea();
+        jTextArea1 = new javax.swing.JTextArea();
         jLabel4 = new javax.swing.JLabel();
-        jTextFieldEnv = new javax.swing.JTextField();
-        jButtonSend = new javax.swing.JButton();
+        jTextField3 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -51,22 +51,18 @@ public class Window extends javax.swing.JFrame {
 
         jLabel3.setText("Port");
 
-        jButtonSetIp.setText("Set Ip");
-        jButtonSetIp.setToolTipText("");
-        jButtonSetIp.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonSetIpActionPerformed(evt);
-            }
-        });
+        jButton1.setText("Connect");
 
-        jTextAreaRec.setEditable(false);
-        jTextAreaRec.setColumns(20);
-        jTextAreaRec.setRows(5);
-        jScrollPane1.setViewportView(jTextAreaRec);
+        jTextArea1.setEditable(false);
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
 
         jLabel4.setText("Terminal");
 
-        jButtonSend.setText("Send");
+        jTextField3.setText("jTextField3");
+
+        jButton2.setText("Send");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -81,14 +77,14 @@ public class Window extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2)
-                            .addComponent(jTextFieldIp, javax.swing.GroupLayout.PREFERRED_SIZE, 256, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 256, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel3)
-                            .addComponent(jTextFieldPort, javax.swing.GroupLayout.PREFERRED_SIZE, 256, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 256, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel4)
-                            .addComponent(jButtonSend)
-                            .addComponent(jButtonSetIp))
+                            .addComponent(jButton2)
+                            .addComponent(jButton1))
                         .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(jTextFieldEnv))
+                    .addComponent(jTextField3))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -101,33 +97,26 @@ public class Window extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextFieldIp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextFieldPort, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButtonSetIp)
+                .addComponent(jButton1)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextFieldEnv, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButtonSend)
+                .addComponent(jButton2)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButtonSetIpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSetIpActionPerformed
-        // TODO add your handling code here:
-        if (jTextFieldIp.getText() != "" && jTextFieldPort.getText() != "") {
-            TcpSocket.setIp(jTextFieldIp.getText(), Integer.parseInt(jTextFieldPort.getText()));
-        }
-    }//GEN-LAST:event_jButtonSetIpActionPerformed
 
     /**
      * @param args the command line arguments
@@ -146,37 +135,36 @@ public class Window extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Window().setVisible(true);
+                new MainWindow().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButtonSend;
-    private javax.swing.JButton jButtonSetIp;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JTextArea jTextAreaRec;
-    private javax.swing.JTextField jTextFieldEnv;
-    private javax.swing.JTextField jTextFieldIp;
-    private javax.swing.JTextField jTextFieldPort;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 }
--- tcpSocket/src/tcpsocket/TCPClient.java
+++ tcpSocket/src/tcpsocket/TCPClient.java
@@ -12,31 +12,28 @@ package tcpsocket;
 import java.io.*;
 import java.net.*;
 
-public class TCPClient {
-
-    public TCPClient(String ip, int puerto) throws Exception {
-        System.out.println("Cliente");
-        String sentence;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket(ip, puerto);
-        //Instanciamos un hilo
-        //HiloRecepcion hiloRx = new HiloRecepcion();
-        //pasmos la referencia del socket
-        //hiloRx.setClientSocket(clientSocket);
-        //hiloRx.start();
-
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        sentence = br.readLine();
-        outToServer.writeBytes(sentence + '\n');
-
-
-        String modifiedSentence = "";
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        modifiedSentence = inFromServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
-        inFromServer.close();
-                outToServer.close();
-        //TODO detener hilo
-        clientSocket.close();
-    }
+public class TCPClient
+{
+ public TCPClient(String ip, int puerto) throws Exception
+ {
+     System.out.println("Cliente");
+  String sentence;
+  
+  BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+  Socket clientSocket = new Socket(ip, puerto);
+  //Instanciamos un hilo
+  HiloRecepcion hiloRx = new HiloRecepcion();
+  //pasmos la referencia del socket
+  hiloRx.setClientSocket(clientSocket);
+  hiloRx.start();
+  
+  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+  sentence = inFromUser.readLine();
+  outToServer.writeBytes(sentence + '\n');
+  
+  
+  //TODO detener hilo
+  clientSocket.close();
+ }
 }
+
